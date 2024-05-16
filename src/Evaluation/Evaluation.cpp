cd3f1e9fe95a8ec11b1d457572576e9a3e598722
sopho-s <woodsnicholas01@gmail.com>
2024-05-16 19:27:24 +0100

    Trying with not using make shared pointer

--- src/Evaluation/Evaluation.cpp
+++ src/Evaluation/Evaluation.cpp
@@ -1,6 +1,6 @@
 #include "Evaluation.h"
 namespace Evaluation {
-    inline float Evaluate(const int (&pieces)[64], const int (&colour)[64]) {
+    inline float Evaluate(const ) {
         for (int i = 0; i < 64; i++) {
             ;
         }
--- src/Evaluation/Evaluation.h
+++ src/Evaluation/Evaluation.h
@@ -1,5 +1,6 @@
 #ifndef EVALUATION_H
 #define EVALUATION_H
+#include "PositionInfo.h"
 namespace Evaluation {
     inline float Evaluate(const int (&pieces)[64], const int (&colour)[64]);
 }
